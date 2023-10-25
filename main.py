b36ba154891f1c83f4745b303383558d669e075e
Adensaparzadeh <Asaparzadeh@ufl.edu>
2023-10-25 19:21:23 -0400

    Aden Saparzadeh

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def decoder(password):
+    list = []
+    for i in range(0, len(password)):
+        hold = int(password[i])
+        hold -= 3
+        if hold < 0:
+            hold = abs(hold)
+        list.append(str(hold))
+    string = ''.join(list)
+    return string
 
 
 
 
 
 
-def decoder(decode):
+def encoder(decode):
     list = []
 
     for i in range(0, len(decode)):
@@ -34,14 +44,13 @@ if __name__ == '__main__':
         choice = input("Please enter an option:")
         if choice == "1":
             user_pass = input("Please enter your password to encode:")
-            
+            new_pass = encoder(user_pass)
             print("Your password has been encoded and stored!")
 
 
         if choice == "2":
-            user_pass = input("Please enter your password to decode:")
-            decoder(user_pass)
-            print("Your password has been encoded and stored!")
+            new_pass1 = decoder(new_pass)
+            print(f"The encoded password is {new_pass}, and the original password is {new_pass1}.")
 
         if choice == "3":
             break
